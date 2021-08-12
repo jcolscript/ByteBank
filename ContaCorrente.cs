15ce5f1a7f81f1b911a0062b0d23ad7f63ca97d8
José Carlos <jcolscript@gmail.com>
2021-08-12 01:07:05 -0300

    Adicionar arquivos de projeto.

---
+++ ContaCorrente.cs
@@ -0,0 +1,64 @@
+﻿namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static int Count { get; private set; }
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        private double _saldo;
+        public double Saldo
+        {
+            get 
+            {
+                return _saldo;
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                this._saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            Count++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(0150, 360987);
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "José Carlos";
+            cliente.CPF = "438.022.044.25";
+            cliente.Profissao = "Engenheiro de Software";
+
+            conta.Saldo = 100;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Titular.CPF);
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(ContaCorrente.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
